0a59856e661d834b0de09bf65a2d63ccb3d1b503
Nicki Padar <nickipadar@gmail.com>
2017-01-13 16:19:41 -0800

    Add interactive banner

---
+++ studio1/script.js
@@ -0,0 +1,44 @@
+//Nicki Padar, 2017
+
+console.log("this is a js comment");
+
+function setup() {
+    var myCanvas = createCanvas(800, 250);
+    myCanvas.parent('mySketch');
+    background(255);
+}
+
+function draw() {
+    // statements here
+    noStroke();
+    if (mouseIsPressed) {
+        fill(255);
+    } else {
+        fill(200);
+    }
+    ellipse(mouseX, mouseY, 50, 50);
+
+    /*color c = color(82, 111, 132); //yellow
+    fill(c);
+    noStroke();
+    rect(250, 50, 300, 150); //body
+
+    c = color(50, 55, 100); //dark blue
+    fill(c);
+    rect(250, 35, 50, 15); //button
+
+    c = color(255); //pink
+    fill(c);
+    //noStroke();
+    rect(490, 60, 50, 25); //flash
+
+    c = color(89, 92, 96); //yellow
+    fill(c);
+    //stroke(2);
+    ellipse(400, 130, 100, 100); //outer circle
+
+    c = color(140, 142, 145); //dark blue
+    fill(c);
+    ellipse(400, 130, 80, 80); //inner circle*/
+
+}
