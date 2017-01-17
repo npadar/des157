24bee3570c78555f90543714e4f0f956f883f3ad
Nicki Padar <npadar@scc2102-17.ucdavis.edu>
2017-01-17 15:30:20 -0800

    change var

--- script.js
+++ script.js
@@ -12,15 +12,20 @@ var rightX = leftX + 70;
 var rightY = leftY + 10;
 var starX = 100;
 var starY = 100;
-PImage img1;
-PImage img2;
-PImage img3;
-PImage img4;
+var img;
+var img2;
+var img3;
+var img4;
 
 
 function setup() {
     var myCanvas = createCanvas(800, 250);
     myCanvas.parent('mySketch');
+    
+    img = loadImage("background-img2.png"); //load image
+    img2 = loadImage("polar-bear.png"); //load image
+    img3 = loadImage("background-img.png"); //load image
+    img4 = loadImage("polar-bear.png"); //load image
 }
 
 function draw() {
@@ -38,19 +43,23 @@ function draw() {
 }
 
 function nightBack() {
-    loadImage("background-img2.png", function (img1) {
+    /*loadImage("background-img2.png", function (img1) {
         image(img1, 0, 0, img.width / 4, img.height / 4);
     });
     loadImage("polar-bear.png", function (img2) {
         image(img2, x + 300, y - 10, img.width / 3, img.height / 3);
-    });
+    });*/
+    image(img, 0, 0, img.width / 4, img.height / 4);
+    image(img2, x + 300, y - 10, img2.width / 3, img2.height / 3);
 }
 
 function dayBack() {
-    loadImage("background-img.png", function (img3) {
+    /*loadImage("background-img.png", function (img3) {
         image(img3, 0, 0, img.width / 4, img.height / 4);
     });
     loadImage("polar-bear.png", function (img4) {
         image(img4, x, y, img.width / 2, img.height / 2);
-    });
+    });*/
+    image(img3, 0, 0, img3.width / 4, img3.height / 4);
+    image(img4, x, y, img4.width / 2, img4.height / 2);
 }
