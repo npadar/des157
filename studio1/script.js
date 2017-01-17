e9c1b824317379cc5a283424be5b3d6eadf043f4
Nicki Padar <nickipadar@gmail.com>
2017-01-17 01:23:16 -0800

    rearrange

--- studio1/script.js
+++ studio1/script.js
@@ -19,21 +19,6 @@ var middleY = leftY - 30;
 var rightX = leftX + 70;
 var rightY = leftY + 10;
 
-function draw() {
-    // statements here
-
-    noStroke();
-    //background(255);
-    fill(219, 212, 212);
-
-    ellipse(leftX, leftY, 80, 60);
-    ellipse(middleX, middleY, 80, 60);
-    ellipse(rightX, rightY, 100, 60);
-    leftX++;
-    middleX++;
-    rightX++;
-}
-
 function nightBack() {
     loadImage("background-img2.png", function (img) {
         image(img, 0, 0, img.width / 4, img.height / 4);
@@ -50,4 +35,19 @@ function dayBack() {
     loadImage("polar-bear.png", function (img) {
         image(img, x, y, img.width / 2, img.height / 2);
     });
+}
+
+function draw() {
+    // statements here
+
+    noStroke();
+    //background(255);
+    fill(219, 212, 212);
+
+    ellipse(leftX, leftY, 80, 60);
+    ellipse(middleX, middleY, 80, 60);
+    ellipse(rightX, rightY, 100, 60);
+    leftX++;
+    middleX++;
+    rightX++;
 }
