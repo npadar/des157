f870c38f7e60f4b4c3f261f265fb30ca6810dc06
Nicki Padar <nickipadar@gmail.com>
2017-01-17 00:53:01 -0800

    test

--- studio1/script.js
+++ studio1/script.js
@@ -5,8 +5,8 @@ console.log("this is a js comment");
 function setup() {
     var myCanvas = createCanvas(800, 250);
     myCanvas.parent('mySketch');
-    myCanvas.mouseOver(night);
-    myCanvas.mouseOut(day);
+    myCanvas.mouseOver(nightBack);
+    myCanvas.mouseOut(dayBack);
 }
 
 var r1 = 153;
@@ -48,42 +48,15 @@ function draw() {
     }
 
     ellipse(mouseX, mouseY, 5, 5);*/
-
-    /*fill(82, 111, 132); //turquoise
-    rect(250, 50, 300, 150); //body
-
-    fill(50, 55, 100); //dark blue
-    rect(250, 35, 50, 15); //button
-
-    fill(255); //white
-    rect(490, 60, 50, 25); //flash
-
-    fill(89, 92, 96); //dark gray
-    ellipse(400, 130, 100, 100); //outer circle
-
-    fill(140, 142, 145); //light gray
-    ellipse(400, 130, 80, 80); //inner circle
-    }
-
-    var value = 0;
-
-    function mouseClicked() {
-        if (value == 0) {
-            background(255);
-        } else {
-            value = 255;
-        }
-    }*/
-    //moveBear(mouseX, mouseY, img);
 }
 
-function night() {
+function nightBack() {
     loadImage("background-img2.png", function (img) {
         image(img, 0, 0, img.width / 4, img.height / 4);
     });
 }
 
-function day() {
+function dayBack() {
     loadImage("background-img.png", function (img) {
         image(img, 0, 0, img.width / 4, img.height / 4);
     });
