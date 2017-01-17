9588089be59ea66982b77f7bdcdce8621dde2f70
Nicki Padar <nickipadar@gmail.com>
2017-01-16 18:05:05 -0800

    change start pos

--- studio1/script.js
+++ studio1/script.js
@@ -13,7 +13,7 @@ function setup() {
     myCanvas.parent('mySketch');
     //background(255);
     loadImage("polar-bear.svg", function (img) {
-        image(img, 0, 0);
+        image(img, 0, 10);
     });
 
 }
