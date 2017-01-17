a2c1e11d26f335a749c5e9b32158d0f57e683f0c
Nicki Padar <npadar@scc2102-17.ucdavis.edu>
2017-01-17 15:18:21 -0800

    organize js

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 //Nicki Padar, 2017
 //interactive banner for studio1 portal
-console.log("this is a js comment");
-
-function setup() {
-    var myCanvas = createCanvas(800, 250);
-    myCanvas.parent('mySketch');
-    myCanvas.mouseOver(nightBack);
-    myCanvas.mouseOut(dayBack);
-}
+console.log("this is a js comment!!");
 
 var x = 100;
 var y = 130;
-
 var leftX = 30;
 var leftY = 80;
 var middleX = leftX + 35;
 var middleY = leftY - 30;
 var rightX = leftX + 70;
 var rightY = leftY + 10;
+var starX = 100;
+var starY = 100;
+
+
+function setup() {
+    var myCanvas = createCanvas(800, 250);
+    myCanvas.parent('mySketch');
+}
+
+function draw() {
+    // statements here
+
+    noStroke();
+    //background(255);
+    fill(255);
+    ellipse(starX, starY, 10, 10);
+    starX++;
+    starY -= .1;
+    
+    myCanvas.mouseOver(nightBack);
+    myCanvas.mouseOut(dayBack);
+}
 
 function nightBack() {
     loadImage("background-img2.png", function (img) {
@@ -35,18 +49,4 @@ function dayBack() {
     loadImage("polar-bear.png", function (img) {
         image(img, x, y, img.width / 2, img.height / 2);
     });
-}
-
-var starX = 100;
-var starY = 100;
-
-function draw() {
-    // statements here
-
-    noStroke();
-    //background(255);
-    fill(255);
-    ellipse(starX, starY, 10, 10);
-    starX++;
-    starY -= .1;
 }
