b1c7622dc17ec8f49ccdaed45d5995179d2e04a9
Nicki Padar <nickipadar@gmail.com>
2017-02-04 01:57:51 -0800

    update studio3

--- studio3/script.js
+++ studio3/script.js
@@ -5,19 +5,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // all other js here
     var lanyard = document.getElementById("lanyard");
+    var hair = document.getElementById("hair");
+    var hawaii = document.getElementById("hawaii");
+    var iceland = document.getElementById("iceland");
+    var clog = document.getElementById("clog");
+    var mickey = document.getElementById("mickey");
+    var italy = document.getElementById("italy");
     var answer = document.getElementById("answer");
     var close = document.getElementById("close");
 
-    lanyard.addEventListener("click", function () {
-      console.log("clicked");
+    //lanyard.addEventListener("click", function () {
+      //console.log("clicked");
       //lanyard.style.backgroundColor = "#111";
-      answer.style.display = "block";
+      //answer.style.display = "block";
+    //});
+
+    hawaii.addEventListener("mouseover", function() {
+      console.log("mouseover hawaii");
+      hawaii.style.transition = "width 4s";
     });
 
-    lanyard.addEventListener("mouseover", function() {
-      console.log("mouseover");
-      lanyard.style.boxShadow = "0px 0px 30px yellow";
+    hawaii.addEventListener("click", function () {
+      console.log("hawaii clicked");
+      answer.style.display = "block";
     })
+
+    iceland.addEventListener("mouseover", function () {
+      console.log("mouseover iceland");
+      iceland.style.transition = "width 4s";
+    });
+
+    clog.addEventListener("mouseover", function () {
+      console.log("mouseover clog");
+      clog.style.transition = "width 4s";
+    });
+
+    mickey.addEventListener("mouseover", function () {
+      console.log("mouseover mickey");
+      mickey.style.transition = "width 4s";
+    });
+
+    italy.addEventListener("mouseover", function () {
+      console.log("mouseover italy");
+      italy.style.transition = "width 4s";
+    });
 
     close.addEventListener("click", function () {
       console.log("closeclicked");
