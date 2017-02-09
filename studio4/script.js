6aff84f3c37e8d265d6b4e9df23e233a0c53d3f9
Nicki Padar <nickipadar@gmail.com>
2017-02-09 14:20:50 -0800

    update studio4

--- studio4/script.js
+++ studio4/script.js
@@ -4,5 +4,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
     // all other js here
+    var film2 = document.getElementById("film2");
+    var timer = document.getElementById("timer");
+    var timer2 = document.getElementById("timer2");
+    var timer3 = document.getElementById("timer3");
+    var timer4 = document.getElementById("timer4");
 
+    var tipTimer;
+
+    film2.addEventListener("mouseover", function () {
+      console.log("mouse over film");
+      tipTimer = setTimeout(showFilmTip, 1000);
+    })
 });
