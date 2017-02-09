9339fa93263eb99f8f57e28595d5e4c057c26238
Nicki Padar <nickipadar@gmail.com>
2017-02-09 15:27:13 -0800

    update studio4

--- studio4/script.js
+++ studio4/script.js
@@ -9,11 +9,47 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var timer2 = document.getElementById("timer2");
     var timer3 = document.getElementById("timer3");
     var timer4 = document.getElementById("timer4");
+    var msg = document.getElementById("msg");
+    var countdown = document.getElementById("countdown");
 
     var tipTimer;
+    var timerInterval;
+    var seconds = 15;
 
     film2.addEventListener("mouseover", function () {
       console.log("mouse over film");
       tipTimer = setTimeout(showFilmTip, 1000);
+    });
+
+    film2.addEventListener("mouseout", function () {
+      console.log("mouse out film");
+      clearTimeout(tipTimer);
+      filmTip.style.opacity = 0;
+    });
+
+    film2.addEventListener("click", function () {
+      console.log("film clicked");
+      msg.style.display = "block";
     })
+
+    function showFilmTip() {
+      console.log("showing msg");
+      filmTip.style.opacity = 1;
+    }
+
+    timer2.addEventListener("click", function () {
+      console.log("orange timer clicked");
+      timerInterval = setInterval(countDown, 1000);
+    })
+
+    function countDown () {
+      if (seconds == 0) {
+        clearInterval(timerInterval);
+      }
+      else {
+        seconds--;
+      }
+      document.getElementById("countdown").innerHTML =
+      "Time Left: " + seconds + " seconds";
+    }
 });
