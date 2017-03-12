cea8e5ffa58db8c4a4ff50b2ef01172cb0bb33b3
Nicki Padar <nickipadar@gmail.com>
2017-03-11 17:58:05 -0800

    try leaflet

--- project/final/script.js
+++ project/final/script.js
@@ -1,198 +1,35 @@
 // JavaScript Document
-
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log("DOM fully loaded and parsed");
 
   // all other js here
-  mapboxgl.accessToken = 'pk.eyJ1IjoibnBhZGFyIiwiYSI6ImNpejY1dmxmdTA1M28ycXF3NWxlNTBoMWQifQ._ksyjUDnYSaz47HApqK2LQ';
-  var loader = document.getElementById('loader');
-  //var map = L.mapbox.map('map', 'mapbox.dark');
-  //map.legendControl.addLegend(document.getElementById('legend').innerHTML);
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  console.log("here");
 
-  var bounds = [
-    [-74.04728500751165, 40.68392799015035], // Southwest coordinates
-    [-73.91058699000139, 40.87764500765852]  // Northeast coordinates
-  ];
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'npadar.cj06109d800512smj99b5z75n',
+    accessToken: 'pk.eyJ1IjoibnBhZGFyIiwiYSI6ImNpejY1dmxmdTA1M28ycXF3NWxlNTBoMWQifQ._ksyjUDnYSaz47HApqK2LQ'
+}).addTo(mymap);
 
-  var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/npadar/ciz92kc6i001n2ss2tfsqtz36',
-      zoom: 1.4,
-      center: [4.899, 20.372]
-  });
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
-  //map.legendControl.addLegend(document.getElementById('legend').innerHTML);
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
 
-//CENTER MAP ON CLICK
-/*  map.on('click', function (e) {
-    // Use queryRenderedFeatures to get features at a click event's point
-    // Use layer option to avoid getting results from other layers
-    var features = map.queryRenderedFeatures(e.point, { layers: ['ce'] });
-    // if there are features within the given radius of the click event,
-    // fly to the location of the click event
-    if (features.length) {
-        // Get coordinates from the symbol and center the map on those coordinates
-        map.flyTo({center: features[0].geometry.coordinates});
-    }
-  });
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
 
-  map.on('click', function (e) {
-    var features2 = map.queryRenderedFeatures(e.point, { layers: ['en'] });
-    if (features2.length) {
-        map.flyTo({center: features2[0].geometry.coordinates});
-    }
-  });
-
-  map.on('click', function (e) {
-    var features3 = map.queryRenderedFeatures(e.point, { layers: ['vu'] });
-    if (features3.length) {
-        map.flyTo({center: features3[0].geometry.coordinates});
-    }
-  });
-
-  map.on('click', function (e) {
-    var features4 = map.queryRenderedFeatures(e.point, { layers: ['nt'] });
-    if (features4.length) {
-        map.flyTo({center: features4[0].geometry.coordinates});
-    }
-  });
-
-  map.on('click', function (e) {
-    var features5 = map.queryRenderedFeatures(e.point, { layers: ['lc'] });
-    if (features5.length) {
-        map.flyTo({center: features5[0].geometry.coordinates});
-    }
-  });
-*/
-//POPUP BOXES when user clicks on dots
-
-  //FEATURE 1: CE
-  map.on('click', function(e) {
-  var features = map.queryRenderedFeatures(e.point, {
-    layers: ['ce'] // replace this with the name of the layer
-  });
-  if (!features.length) {
-    return;
-  }
-  var feature = features[0];
-
-  var popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(feature.geometry.coordinates)
-    .setHTML('<h3 title="World Wildlife Fund">' + feature.properties.Name + '</h3><p>' + feature.properties.Status + '</p><p>' +
-    feature.properties.Description + '</p><img src="' + feature.properties.Image + '" alt="image" title="World Wildlife Fund">')
-    //.setLngLat(feature.geometry.coordinates)
-    .addTo(map);
-  });
-
-  //FEATURES 2: EN
-  map.on('click', function(e) {
-    var features2 = map.queryRenderedFeatures(e.point, {
-      layers: ['en']
-    });
-    if (!features2.length) {
-      return;
-    }
-
-    var feature2 = features2[0];
-
-    var popup = new mapboxgl.Popup({ offset: [0, -15] })
-      .setLngLat(feature2.geometry.coordinates)
-      .setHTML('<h3 title="World Wildlife Fund">' + feature2.properties.Name + '</h3><p>' + feature2.properties.Status + '</p><p>' +
-      feature2.properties.Description + '</p><img src="' + feature2.properties.Image + '" alt="image" title="World Wildlife Fund">')
-      //.setLngLat(feature.geometry.coordinates)
-      .addTo(map);
-    });
-
-    //FEATURES 3:VU
-    map.on('click', function(e) {
-      var features3 = map.queryRenderedFeatures(e.point, {
-        layers: ['vu']
-      });
-      if (!features3.length) {
-        return;
-      }
-
-      var feature3 = features3[0];
-
-      var popup = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(feature3.geometry.coordinates)
-        .setHTML('<h3 title="World Wildlife Fund">' + feature3.properties.Name + '</h3><p>' + feature3.properties.Status + '</p><p>' +
-        feature3.properties.Description + '</p><img src="' + feature3.properties.Image + '" alt="image" title="World Wildlife Fund">')
-        //.setLngLat(feature.geometry.coordinates)
-        .addTo(map);
-      });
-
-    //FEATURES 4:NT
-    map.on('click', function(e) {
-      var features4 = map.queryRenderedFeatures(e.point, {
-        layers: ['nt']
-      });
-      if (!features4.length) {
-        return;
-      }
-
-      var feature4 = features4[0];
-
-      var popup = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(feature4.geometry.coordinates)
-        .setHTML('<h3 title="World Wildlife Fund">' + feature4.properties.Name + '</h3><p>' + feature4.properties.Status + '</p><p>' +
-        feature4.properties.Description + '</p><img src="' + feature4.properties.Image + '" alt="image" title="World Wildlife Fund">')
-        //.setLngLat(feature.geometry.coordinates)
-        .addTo(map);
-      });
-
-    //FEATURES 5: LC
-    map.on('click', function(e) {
-      var features5 = map.queryRenderedFeatures(e.point, {
-        layers: ['lc']
-      });
-      if (!features5.length) {
-        return;
-      }
-
-      var feature5 = features5[0];
-
-      var popup = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(feature5.geometry.coordinates)
-        .setHTML('<h3 title="World Wildlife Fund">' + feature5.properties.Name + '</h3><p>' + feature5.properties.Status + '</p><p>' +
-        feature5.properties.Description + '</p><img src="' + feature5.properties.Image + '" alt="image" title="World Wildlife Fund">')
-        //.setLngLat(feature.geometry.coordinates)
-        .addTo(map);
-      });
-
-    //map.legendControl.addLegend(document.getElementById('legend').innerHTML);
-
-    var toggleableLayerIds = [ 'ce', 'en', 'vu', 'nt', 'lc' ]; //all the layers
-    console.log(toggleableLayerIds);
-
-    for (var i = 0; i < toggleableLayerIds.length; i++) { //loop through all layers
-    var id = toggleableLayerIds[i];
-    console.log(id);
-
-    var link = document.createElement('a'); //creates an <a>
-    link.href = '#';
-    link.className = 'active';
-    link.textContent = id;
-
-    link.onclick = function (e) {
-        var clickedLayer = this.textContent;
-        console.log("clickedLayer");
-        e.preventDefault();
-        e.stopPropagation();
-
-        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
-
-        if (visibility === 'visible') {
-            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-            this.className = '';
-        } else {
-            this.className = 'active';
-            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
-        }
-    };
-
-    var layers = document.getElementById('menu');
-    layers.appendChild(link);
-  }
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
 
 }); //end
--- project/proto3/script.js
+++ project/proto3/script.js
@@ -1,152 +1,128 @@
 // JavaScript Document
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("DOM fully loaded and parsed");
+    console.log("DOM fully loaded and parsed");
 
-  // all other js here
-  mapboxgl.accessToken = 'pk.eyJ1IjoibnBhZGFyIiwiYSI6ImNpejY1dmxmdTA1M28ycXF3NWxlNTBoMWQifQ._ksyjUDnYSaz47HApqK2LQ';
-  var loader = document.getElementById('loader');
+    // all other js here
+    mapboxgl.accessToken = 'pk.eyJ1IjoibnBhZGFyIiwiYSI6ImNpejY1dmxmdTA1M28ycXF3NWxlNTBoMWQifQ._ksyjUDnYSaz47HApqK2LQ';
+    var loader = document.getElementById('loader');
 
-  var bounds = [
-    [-74.04728500751165, 40.68392799015035], // Southwest coordinates
-    [-73.91058699000139, 40.87764500765852]  // Northeast coordinates
-  ];
+    var bounds = [
+        [-74.04728500751165, 40.68392799015035], // Southwest coordinates
+        [-73.91058699000139, 40.87764500765852] // Northeast coordinates
+    ];
 
-  var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/npadar/ciz92kc6i001n2ss2tfsqtz36',
-      zoom: 1.4,
-      center: [4.899, 20.372]
-  });
-
-//POPUP BOXES when user clicks on dots
-
-  //FEATURE 1: CE
-  map.on('click', function(e) {
-  var features = map.queryRenderedFeatures(e.point, {
-    layers: ['ce'] // replace this with the name of the layer
-  });
-  if (!features.length) {
-    return;
-  }
-  var feature = features[0];
-
-  var popup = new mapboxgl.Popup({ offset: [0, -15] })
-    .setLngLat(feature.geometry.coordinates)
-    .setHTML('<h3 title="World Wildlife Fund">' + feature.properties.Name + '</h3><p>' + feature.properties.Status + '</p><p>' +
-    feature.properties.Description + '</p><img src="' + feature.properties.Image + '" alt="image" title="World Wildlife Fund">')
-    .addTo(map);
-  });
-
-  //FEATURES 2: EN
-  map.on('click', function(e) {
-    var features2 = map.queryRenderedFeatures(e.point, {
-      layers: ['en']
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/npadar/ciz92kc6i001n2ss2tfsqtz36',
+        zoom: 0.9,
+        center: [4.899, 20.372]
     });
-    if (!features2.length) {
-      return;
-    }
 
-    var feature2 = features2[0];
+    //POPUP BOXES when user clicks on dots
 
-    var popup = new mapboxgl.Popup({ offset: [0, -15] })
-      .setLngLat(feature2.geometry.coordinates)
-      .setHTML('<h3 title="World Wildlife Fund">' + feature2.properties.Name + '</h3><p>' + feature2.properties.Status + '</p><p>' +
-      feature2.properties.Description + '</p><img src="' + feature2.properties.Image + '" alt="image" title="World Wildlife Fund">')
-      .addTo(map);
+    //FEATURE 1: CE
+    map.on('click', function(e) {
+        var features = map.queryRenderedFeatures(e.point, {
+            layers: ['ce'] // replace this with the name of the layer
+        });
+        if (!features.length) {
+            return;
+        }
+        var feature = features[0];
+
+        var popup = new mapboxgl.Popup({
+                offset: [0, -15]
+            })
+            .setLngLat(feature.geometry.coordinates)
+            .setHTML('<h3 title="World Wildlife Fund">' + feature.properties.Name + '</h3><p>' + feature.properties.Status + '</p><p>' +
+                feature.properties.Description + '</p><img src="' + feature.properties.Image + '" alt="image" title="World Wildlife Fund">')
+            .addTo(map);
+    });
+
+    //FEATURES 2: EN
+    map.on('click', function(e) {
+        var features2 = map.queryRenderedFeatures(e.point, {
+            layers: ['en']
+        });
+        if (!features2.length) {
+            return;
+        }
+
+        var feature2 = features2[0];
+
+        var popup = new mapboxgl.Popup({
+                offset: [0, -15]
+            })
+            .setLngLat(feature2.geometry.coordinates)
+            .setHTML('<h3 title="World Wildlife Fund">' + feature2.properties.Name + '</h3><p>' + feature2.properties.Status + '</p><p>' +
+                feature2.properties.Description + '</p><img src="' + feature2.properties.Image + '" alt="image" title="World Wildlife Fund">')
+            .addTo(map);
     });
 
     //FEATURES 3:VU
     map.on('click', function(e) {
-      var features3 = map.queryRenderedFeatures(e.point, {
-        layers: ['vu']
-      });
-      if (!features3.length) {
-        return;
-      }
+        var features3 = map.queryRenderedFeatures(e.point, {
+            layers: ['vu']
+        });
+        if (!features3.length) {
+            return;
+        }
 
-      var feature3 = features3[0];
+        var feature3 = features3[0];
 
-      var popup = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(feature3.geometry.coordinates)
-        .setHTML('<h3 title="World Wildlife Fund">' + feature3.properties.Name + '</h3><p>' + feature3.properties.Status + '</p><p>' +
-        feature3.properties.Description + '</p><img src="' + feature3.properties.Image + '" alt="image" title="World Wildlife Fund">')
-        .addTo(map);
-      });
+        var popup = new mapboxgl.Popup({
+                offset: [0, -15]
+            })
+            .setLngLat(feature3.geometry.coordinates)
+            .setHTML('<h3 title="World Wildlife Fund">' + feature3.properties.Name + '</h3><p>' + feature3.properties.Status + '</p><p>' +
+                feature3.properties.Description + '</p><img src="' + feature3.properties.Image + '" alt="image" title="World Wildlife Fund">')
+            .addTo(map);
+    });
 
     //FEATURES 4:NT
     map.on('click', function(e) {
-      var features4 = map.queryRenderedFeatures(e.point, {
-        layers: ['nt']
-      });
-      if (!features4.length) {
-        return;
-      }
+        var features4 = map.queryRenderedFeatures(e.point, {
+            layers: ['nt']
+        });
+        if (!features4.length) {
+            return;
+        }
 
-      var feature4 = features4[0];
+        var feature4 = features4[0];
 
-      var popup = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(feature4.geometry.coordinates)
-        .setHTML('<h3 title="World Wildlife Fund">' + feature4.properties.Name + '</h3><p>' + feature4.properties.Status + '</p><p>' +
-        feature4.properties.Description + '</p><img src="' + feature4.properties.Image + '" alt="image" title="World Wildlife Fund">')
-        .addTo(map);
-      });
+        var popup = new mapboxgl.Popup({
+                offset: [0, -15]
+            })
+            .setLngLat(feature4.geometry.coordinates)
+            .setHTML('<h3 title="World Wildlife Fund">' + feature4.properties.Name + '</h3><p>' + feature4.properties.Status + '</p><p>' +
+                feature4.properties.Description + '</p><img src="' + feature4.properties.Image + '" alt="image" title="World Wildlife Fund">')
+            .addTo(map);
+    });
 
     //FEATURES 5: LC
-    var info = document.getElementById('info');
-
-    var content = '<div class="tabs">' +
-          '<div class="tab" id="tab-1">' +
-          '<div class="content">' +
-          '<b>Tab 1 content</b>' +
-          '</div>' +
-          '</div>' +
-
-          '<div class="tab" id="tab-2">' +
-          '<div class="content">' +
-          '<b>Tab 2 content</b>' +
-          '</div>' +
-          '</div>' +
-
-          '<div class="tab" id="tab-3">' +
-          '<div class="content">' +
-          '<b>Tab 3 content</b>' +
-          '</div>' +
-          '</div>' +
-
-          '<ul class="tabs-link">' +
-          '<li class="tab-link"> <a href="#tab-1"><span>Tab 1</span></a></li>' +
-          '<li class="tab-link"> <a href="#tab-2"><span>Tab 2</span></a></li>' +
-          '<li class="tab-link"> <a href="#tab-3"><span>Tab 3</span></a></li>' +
-          '</ul>' +
-      '</div>';
-
-      var polygon1 = L.circle([51.508, -0.11], 500, {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5
-      }).addTo(map).bindPopup(content);
-
     map.on('click', function(e) {
-      var features5 = map.queryRenderedFeatures(e.point, {
-        layers: ['lc']
-      });
-      if (!features5.length) {
-        return;
-      }
+        var features5 = map.queryRenderedFeatures(e.point, {
+            layers: ['lc']
+        });
+        if (!features5.length) {
+            return;
+        }
 
-      var feature5 = features5[0];
+        var feature5 = features5[0];
 
-      var popup = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(feature5.geometry.coordinates)
-        .setHTML('<h3 title="World Wildlife Fund">' + feature5.properties.Name + '</h3><p>' + feature5.properties.Status + '</p><p>' +
-        feature5.properties.Description + '</p><img src="' + feature5.properties.Image + '" alt="image" title="World Wildlife Fund">')
-        .addTo(map);
-      });
+        var popup = new mapboxgl.Popup({
+                offset: [0, -15]
+            })
+            .setLngLat(feature5.geometry.coordinates)
+            .setHTML('<h3 title="World Wildlife Fund">' + feature5.properties.Name + '</h3><p>' + feature5.properties.Status + '</p><p>' +
+                feature5.properties.Description + '</p><img src="' + feature5.properties.Image + '" alt="image" title="World Wildlife Fund">')
+            .addTo(map);
+    });
 
-      //myLayer.eachLayer(function (marker) {
-      //  var p = marker.feature.properties;
-      //}
+    //myLayer.eachLayer(function (marker) {
+    //  var p = marker.feature.properties;
+    //}
     //map.legendControl.addLegend(document.getElementById('legend').innerHTML);
 
     /*var toggleableLayerIds = [ 'ce', 'en', 'vu', 'nt', 'lc' ]; //all the layers
